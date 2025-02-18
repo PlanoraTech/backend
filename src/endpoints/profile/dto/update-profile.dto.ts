6a623bf3390ee6125ca397db827578d061a57a76
Th3B3n1 <othersc9bst@gmail.com>
2025-02-18 15:17:31 +0100

    validation for presentators; validation for login; validation for update-profile; removed profile dto

--- src/endpoints/profile/dto/update-profile.dto.ts
+++ src/endpoints/profile/dto/update-profile.dto.ts
@@ -1,4 +1,15 @@
+import { IsNotEmpty, IsString, Length, Matches } from "class-validator";
+
 export class UpdateProfileDto {
+    @IsString()
+    @IsNotEmpty()
     oldPassword: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @Length(8, 128)
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,128}$/, {
+        message: 'Password too weak. Must contain at least one uppercase letter, one lowercase letter, one number, and one special character.'
+    })
     newPassword: string;
 }
