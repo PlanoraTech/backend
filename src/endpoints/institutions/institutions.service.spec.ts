96f03377538105fb98b5313b74f627be5fbb1f94
Th3B3n1 <othersc9bst@gmail.com>
2025-02-19 14:12:34 +0100

    tests for institutions; test for PrismaService

--- src/endpoints/institutions/institutions.service.spec.ts
+++ src/endpoints/institutions/institutions.service.spec.ts
@@ -1,19 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '@app/prisma/prisma.service';
 import { InstitutionsService } from './institutions.service';
+import { Institutions } from '@prisma/client';
 
 describe('InstitutionsService', () => {
-  let service: InstitutionsService;
+	let service: InstitutionsService;
+	let prisma: PrismaService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [InstitutionsService, PrismaService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [InstitutionsService, PrismaService],
+		}).compile();
 
-    service = module.get<InstitutionsService>(InstitutionsService);
-  });
+		service = module.get<InstitutionsService>(InstitutionsService);
+		prisma = module.get<PrismaService>(PrismaService);
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
+
+	it('should return all institutions', async () => {
+		let institutions: Institutions[] = await prisma.institutions.createManyAndReturn({
+			data: [
+				{
+					id: undefined,
+					name: 'Test Institution 1',
+					type: 'SCHOOL',
+					access: 'PUBLIC',
+					color: '#FFFFFFF',
+					website: 'https://test.institution1.com'
+				},
+				{
+					id: undefined,
+					name: 'Test Institution 2',
+					type: 'SCHOOL',
+					access: 'PRIVATE',
+					color: '#000000',
+					website: 'https://test.institution1.com'
+				},
+			],
+		});
+
+		expect(await service.findAll()).toBe(institutions);
+	});
+
+	it('should return an institutions', async () => {
+		let institutions: Institutions[] = await prisma.institutions.createManyAndReturn({
+			data: [
+				{
+					id: undefined,
+					name: 'Test Institution 1',
+					type: 'SCHOOL',
+					access: 'PUBLIC',
+					color: '#FFFFFFF',
+					website: 'https://test.institution1.com'
+				},
+				{
+					id: undefined,
+					name: 'Test Institution 2',
+					type: 'SCHOOL',
+					access: 'PRIVATE',
+					color: '#000000',
+					website: 'https://test.institution1.com'
+				},
+			],
+		});
+
+		expect(await service.findOne(institutions[0].id)).toBe(institutions[0].id);
+	});
 });
