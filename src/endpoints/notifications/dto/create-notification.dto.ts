df8b611bd0fcd64f9948d27ff9763c58b8cb1a25
Th3B3n1 <othersc9bst@gmail.com>
2025-03-20 10:01:22 +0100

    validation removed temporarily

--- src/endpoints/notifications/dto/create-notification.dto.ts
+++ src/endpoints/notifications/dto/create-notification.dto.ts
@@ -1,10 +1,7 @@
-import { IsNotEmpty, IsString, Matches } from "class-validator";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateNotificationDto {
     @IsString()
     @IsNotEmpty()
-    @Matches(/^[a-z\d]{8}-[a-z\d]{4}-[a-z\d]{4}-[a-z\d]{4}-[a-z\d]{12}$/i, {
-        message: "Expo push token must be a valid UUID"
-    })
     expoPushToken: string;
 }
