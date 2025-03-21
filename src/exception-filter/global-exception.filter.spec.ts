7fc0d9d099ad2d5449611d719cdbabcced516744
Th3B3n1 <othersc9bst@gmail.com>
2025-03-21 18:28:27 +0100

    removed Logger reference from GlobalExceptionFilter's test file

--- src/exception-filter/global-exception.filter.spec.ts
+++ src/exception-filter/global-exception.filter.spec.ts
@@ -1,8 +1,7 @@
-import { Logger } from '@nestjs/common';
 import { GlobalExceptionFilter } from './global-exception.filter';
 
 describe('GlobalExceptionFilter', () => {
   it('should be defined', () => {
-    expect(new GlobalExceptionFilter(new Logger)).toBeDefined();
+    expect(new GlobalExceptionFilter).toBeDefined();
   });
 });
