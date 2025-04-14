c70485f742487dd40731c8ae65d141fa3ad1faeb
Th3B3n1 <othersc9bst@gmail.com>
2025-04-14 23:35:51 +0200

    refactor: move ExtendedAppointments interfaces to a new file for better organization

---
+++ src/endpoints/appointments/interfaces/extendedappointments.interface.ts
@@ -0,0 +1,33 @@
+import { Appointments } from "@prisma/client";
+
+export interface ExtendedAppointments extends Appointments {
+    subject: {
+        id: string;
+        name: string;
+        subjectId: string;
+    };
+    rooms: {
+        id: string;
+        name: string;
+    }[];
+    timetables: {
+        id: string;
+        name: string;
+    }[];
+    presentators: {
+        id: string;
+        name: string;
+        isSubstituted: boolean;
+    }[];
+}
+
+export interface ExtendedAppointmentsWithPrismaPresentators
+    extends Omit<ExtendedAppointments, 'presentators'> {
+    presentators: {
+        presentator: {
+            id: string;
+            name: string;
+        };
+        isSubstituted: boolean;
+    }[];
+}
